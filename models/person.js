387b7509dcc03340973a652ed0b97b1732200514
quntaoz <quntaozheng@gmail.com>
2024-01-11 00:08:46 -0500

    uibuild

--- models/person.js
+++ models/person.js
@@ -22,8 +22,14 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minLength: 3,
-    required: true
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
   }
 })
 
